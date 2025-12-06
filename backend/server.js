8998a610260fae6621e962425cf5e56490c32c0c
Aryan-986 <aryankarki984@gmail.com>
2025-12-07 00:51:21 +0545

    push1

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ import orderRouter from './routes/orderRoute.js'
 
 // App Config
 const app = express()
-const port = process.env.PORT || 4000
+
+// Connect to DB and Cloudinary
 connectDB()
 connectCloudinary()
 
@@ -19,13 +20,14 @@ app.use(express.json())
 app.use(cors())
 
 // api endpoints
-app.use('/api/user',userRouter)
-app.use('/api/product',productRouter)
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("API Working")
 })
 
-app.listen(port, ()=> console.log('Server started on PORT : '+ port))
+// Export the Express app for Vercel
+export default app
